ed1ec85198ac590e2cd8336ba143432f04988d77
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-03-28 12:37:57 -0600

    Added View files

---
+++ src/drawing/view/DrawingPanel.java
@@ -0,0 +1,20 @@
+package drawing.view;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import drawing.controller.*;
+
+public class DrawingPanel extends JPanel {
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private DrawingController app;
+	
+	
+}
