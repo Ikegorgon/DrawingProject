96827c9094fc12d309d4f8b503d2eb3bba15a1e9
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-04-26 12:33:41 -0600

    Made Drawing Frame

--- src/drawing/view/DrawingFrame.java
+++ src/drawing/view/DrawingFrame.java
@@ -1,5 +1,21 @@
 package drawing.view;
 
-public class DrawingFrame {
+import javax.swing.*;
+import drawing.controller.*;
+
+public class DrawingFrame extends JFrame {
+	private DrawingPanel appPanel;
 	
+	public DrawingFrame(DrawingController app) {
+		super();
+		appPanel = new DrawingPanel(app);
+		setupFrame();
+	}
+	private void setupFrame() {
+		this.setSize(1200, 700);
+		this.setContentPane(appPanel);
+		this.setTitle("Art in Java");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
 }
