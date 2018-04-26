4a17ebd2ffa05e0e1ee5cc7f0df8ddbbf5c2dfae
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-04-26 13:51:33 -0600

    Controller Made and Completed

--- src/drawing/controller/DrawingController.java
+++ src/drawing/controller/DrawingController.java
@@ -1,7 +1,26 @@
 package drawing.controller;
 
+import java.awt.Component;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import drawing.view.*;
+import drawing.model.*;
+
 public class DrawingController {
+	private DrawingFrame frame;
+	public DrawingController() {
+		frame = new DrawingFrame(this);
+	}
 	public void start() {
-		
+		JOptionPane.showMessageDialog(frame, "Welcome to Art!");
+	}
+	public DrawingFrame getFrame() {
+		return frame;
+	}
+
+	public void handleErrors(IOException error) {
+		JOptionPane.showMessageDialog(frame, error.getMessage());
 	}
 }
